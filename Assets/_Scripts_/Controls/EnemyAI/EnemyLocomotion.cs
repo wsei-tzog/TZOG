7dc1d5bab2e27ab884276073252e148631e3cd1c
dawid.rokos@gmail.com <dawid.rokos@gmail.com>
2023-02-04 14:29:50 +0100

    add new gates

--- Assets/_Scripts_/Controls/EnemyAI/EnemyLocomotion.cs
+++ Assets/_Scripts_/Controls/EnemyAI/EnemyLocomotion.cs
@@ -6,11 +6,74 @@ using UnityEngine.AI;
 public class EnemyLocomotion : MonoBehaviour
 {
 
+    /// <summary>
+    /// NEW AI
+    /// </summary>
+
+    // The target transform (i.e. the player)
+    public Transform target;
+
+    // The range at which the enemy can see the player
+    public float detectionRange = 10f;
+
+    // The range at which the enemy will stop chasing the player
+    public float loseSightRange = 15f;
+
+    // The speed at which the enemy moves towards the player
+    public float moveSpeed = 3f;
+
+    // The distance at which the enemy will attack the player
+    public float attackRange = 1.5f;
+
+    // The amount of damage the enemy does when it attacks
+    public int attackDamage = 10;
+
+    // The rate at which the enemy can attack
+    public float attackRate = 1f;
+
+    // The enemy's field of view angle (in degrees)
+    public float fieldOfViewAngle = 110f;
+
+    // A reference to the enemy's Animator component
+    private Animator animator;
+
+    // A reference to the enemy's NavMeshAgent component
+    private NavMeshAgent navMeshAgent;
+
+    // A timer for keeping track of the enemy's attack rate
+    private float attackTimer;
+
+
+    /// <summary>
+    /// Patrol
+    /// </summary>
+    public Transform[] patrolPoints;
+    private int currentPoint;
+    public float rotationSpeed;
+    // public float fieldOfViewAngle;
+    public bool playerInSight;
+    public Vector3 personalLastSighting;
+    private SphereCollider col;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public bool detectedPlayer, Alive;
     public bool isMoving;
 
 
-    public Animator animator;
+    // public Animator animator;
     public NavMeshAgent agent;
     public Transform playerTransform;
     Transform mTransform;
@@ -24,6 +87,15 @@ public class EnemyLocomotion : MonoBehaviour
 
     private void Start()
     {
+        currentPoint = 0;
+        transform.position = patrolPoints[currentPoint].position;
+        col = GetComponent<SphereCollider>();
+
+
+        // Get reference to Animator and NavMeshAgent components
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
 
         Alive = true;
         mTransform = this.transform;
@@ -31,10 +103,43 @@ public class EnemyLocomotion : MonoBehaviour
 
     private void Update()
     {
+        // Calculate distance to target
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
         float distanceToPlayer = Vector3.Distance(mTransform.position, playerTransform.position);
+
+
 
         if (Alive)
         {
+            if (distanceToTarget < detectionRange && IsInFieldOfView())
+            {
+                // Set the enemy's destination to the player's position
+                navMeshAgent.SetDestination(target.position);
+
+                // If the player is within the attack range
+                if (distanceToTarget < attackRange)
+                {
+                    // Attack the player
+                    Attack();
+                }
+                // If the player is outside the attack range
+                else
+                {
+                    // Set the enemy's animation state to "walking"
+                    animator.SetBool("IsWalking", true);
+                }
+            }
+            // If the player is outside the lose sight range
+            else if (distanceToTarget > loseSightRange)
+            {
+                // Reset the enemy's destination
+                navMeshAgent.SetDestination(transform.position);
+
+                // Set the enemy's animation state to "idle"
+                animator.SetBool("IsWalking", false);
+            }
+
+
             if (detectedPlayer)
             {
                 MoveToPlayer(distanceToPlayer);
@@ -47,6 +152,43 @@ public class EnemyLocomotion : MonoBehaviour
         }
 
     }
+
+    // method for patroling
+    // method when player spotted
+
+    private bool IsInFieldOfView()
+    {
+        // Calculate the angle between the enemy's forward direction and the direction to the player
+        float angleToTarget = Vector3.Angle(transform.forward, target.position - transform.position);
+
+        // Return true if the angle is within the enemy's field of view
+        return angleToTarget < fieldOfViewAngle * 0.5f;
+    }
+
+
+    private void Attack()
+    {
+        // If the attack timer has reached the attack rate
+        if (attackTimer >= attackRate)
+        {
+            // Apply damage to the player
+            // target.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+
+            // Reset the attack timer
+            attackTimer = 0f;
+        }
+        // If the attack timer has not yet reached the attack rate
+        else
+        {
+            // Increment the attack timer
+            attackTimer += Time.deltaTime;
+        }
+    }
+
+
+
+
+
 
     public void PlayerDetecion(float distanceToPlayer)
     {
